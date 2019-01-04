2624c1282c40f9d2a6489d2cd109eee8df7b031c
John C. Lendvoy <johntheleatherman@gmail.com>
2019-01-03 21:02:59 -0700

    fixing font on home page

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -20,7 +20,7 @@
 
 	@component('components.cta_button')
 		@slot('link', '/leather')
-		@slot('text', 'Shop All Products')
+		@slot('text', 'Browse Leather')
 	@endcomponent
 
 	@component('components.title_row')
@@ -31,19 +31,19 @@
 		<div class="col-md-4 text-center mt20">
 			@include('svg.design', ['height' => '100px'])
 			<h4 class="mt20">Original Designs</h4>
-			<p class="mt10 text-muted">Each design begins its life as an original sketch.</p>
+			<p class="mt10 text-dk-grey">Each design begins its life as an original sketch.</p>
 		</div>
 
 		<div class="col-md-4 text-center mt20">
 			@include('svg.handmade', ['height' => '100px'])
 			<h4 class="mt20">Hand Crafted</h4>
-			<p class="mt10 text-muted">My work is made with care using only hand tools.</p>
+			<p class="mt10 text-dk-grey">My work is made with care using only hand tools.</p>
 		</div>
 
 		<div class="col-md-4 text-center mt20">
 			@include('svg.unique', ['height' => '100px'])
 			<h4 class="mt20">Unique Products</h4>
-			 <p class="mt10 text-muted">Every thing I make is one-of-a-kind.</p>
+			 <p class="mt10 text-dk-grey">Every thing I make is one-of-a-kind.</p>
 		</div>
 	</div>
 
