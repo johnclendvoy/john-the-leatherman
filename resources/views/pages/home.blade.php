88e5fdddf81c875add28db04eac5c2f5a73e113b
John C. Lendvoy <johntheleatherman@gmail.com>
2019-04-20 21:41:49 -0600

    Updated: process section on home page

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,14 +1,20 @@
 @extends('layouts.public')
 
 @section('banner')
-<div class="home-banner text-center">
+<div class="d-none d-md-block home-banner text-center">
 	<div class="home-banner-text">
 		<div class="home-title">John</div>
 		<div class="home-subtitle">- The -</div>
 		<div class="home-title">Leatherman</div>
 	</div>
 </div>
+
+<div class="d-block d-md-none">
+	<img class="img img-fluid sm-banner-image" src="/images/home/coasters-banner.jpg">
+</div>
+
 @stop
+
 
 @section('content')
 
@@ -20,37 +26,56 @@
 
 	@component('components.cta_button')
 		@slot('link', '/leather')
-		@slot('text', 'Browse Leather')
+		@slot('text', 'Browse All Products')
 	@endcomponent
 
-	@component('components.title_row')
-		@slot('title', 'The Process')
-	@endcomponent
+	<div class="row full-width green-box textured-bg">
+		<div class="col-12">
+			<div class="container">
 
-	<div class="row">
-		<div class="col-md-4 text-center mt20">
-			@include('svg.design', ['height' => '100px'])
-			<h4 class="mt20">Original Designs</h4>
-			<p class="mt10 text-dk-grey">Each design begins its life as an original sketch.</p>
-		</div>
+				@component('components.title_row')
+					@slot('title', 'The Process')
+				@endcomponent
 
-		<div class="col-md-4 text-center mt20">
-			@include('svg.handmade', ['height' => '100px'])
-			<h4 class="mt20">Hand Crafted</h4>
-			<p class="mt10 text-dk-grey">My work is made with care using only hand tools.</p>
-		</div>
+				<div class="row">
+					<div class="col-md-4 text-center mt20">
+						{{-- @include('svg.design', ['height' => '100px']) --}}
+						<div class="px-5">
+							<img class="img img-fluid rounded-circle" src="/images/process/sketched_crawdad.png">
+						</div>
+						<h4 class="mt20">Original Designs</h4>
+						<p class="mt10 text-dk-grey">Each design begins its life as an original sketch.</p>
+					</div>
 
-		<div class="col-md-4 text-center mt20">
-			@include('svg.unique', ['height' => '100px'])
-			<h4 class="mt20">Unique Products</h4>
-			 <p class="mt10 text-dk-grey">Every thing I make is one-of-a-kind.</p>
+					<div class="col-md-4 text-center mt20">
+						{{-- @include('svg.handmade', ['height' => '100px']) --}}
+						<div class="px-5">
+							<img class="img img-fluid rounded-circle" src="/images/process/tooled_crawdad.png">
+						</div>
+						<h4 class="mt20">Hand Crafted</h4>
+						<p class="mt10 text-dk-grey">My work is made with care using only hand tools.</p>
+					</div>
+
+					<div class="col-md-4 text-center mt20">
+						{{-- @include('svg.unique', ['height' => '100px']) --}}
+						<div class="px-5">
+							<img class="img img-fluid rounded-circle" src="/images/process/finished_crawdad.png">
+						</div>
+						<h4 class="mt20">Unique Products</h4>
+						 <p class="mt10 text-dk-grey">Every thing I make is one-of-a-kind.</p>
+					</div>
+				</div>
+
+				@component('components.cta_button')
+					@slot('link', '/about')
+					@slot('text', 'Learn More')
+				@endcomponent
+				
+			</div>
 		</div>
 	</div>
 
-	@component('components.cta_button')
-		@slot('link', '/about')
-		@slot('text', 'Learn More')
-	@endcomponent
+
 
 	@unless(empty($testimonials))
 
