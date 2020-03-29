338df0a764cd94552bcbedf3f02dc21f361b6627
John C. Lendvoy <johntheleatherman@gmail.com>
2020-03-29 12:28:54 -0600

    Added jcl logo to footer, composer update

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -19,7 +19,19 @@
 
 		<div class="row mt10">
 				<div class="col-sm-8 col-xs-12 text-center text-sm-left">
-				<span class="text-md-grey">&copy; {{ date('Y') }}, <a class="text-md-grey" target="_blank" href="http://johnclendvoy.ca">John C. Lendvoy</a></span>
+					<span class="text-md-grey">&copy; {{ date('Y') }}, John C. Lendvoy</span>
+				</div>
+
+				<div class="col-sm-4 col-xs-12 text-center text-sm-right">
+					<a class="jcl-link" href="//johnclendvoy.ca" title="John C. Lendvoy Software Development">
+						<div class="small">Website By</div>
+						<svg height="24px" viewBox="0 0 240 90" class="jcl-logo" xmlns="http://www.w3.org/2000/svg" xmlns:svg="http://www.w3.org/2000/svg">
+							<polygon points="50,0 90,0 100,17.32 60,86.6 0,86.6 10.00,69.28 0,51.96 20,51.96 30,69.28 60,17.32"/>
+							<polygon points="110,0 170,0 180,17.32 170,34.64 150,34.64 140, 51.96 150,69.28 140,86.6 80,86.6 120,17.32"/>
+							<polygon points="190,0 230,0 240,17.32 220,51.96 230,69.28 220,86.6 160,86.6 200,17.32"/>
+						</svg>
+					</a>
+				</div>
 			</div>
 		</div>
 
